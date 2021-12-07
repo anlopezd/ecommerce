8f09c1b9f83e7be4fe8677b52c4777aa2a8fae95
anlopezd <91441753+anlopezd@users.noreply.github.com>
2021-12-06 21:53:37 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 import countdown from "./time.js";
 
-const lessBtn = document.querySelector(".less")
-const plusBtn = document.querySelector(".plus")
-const number = document.querySelector(".number")
-const price = document.querySelector(".price")
+const lessBtn = document.querySelector(".less");
+const plusBtn = document.querySelector(".plus");
+const number = document.querySelector(".number");
+const price = document.querySelector(".price");
 let value = 1;
 
 lessBtn.addEventListener("click", () => {
-    value--; 
-    value < 1 ? value = 0 : value
+    value--;
+    value < 1 ? value = 0 : value;
     number.innerHTML = value;
     price.innerHTML = `$${20 * value}`
 })
 
 
 plusBtn.addEventListener("click", () => {
-value++
+value++;
     number.innerHTML = value;
     price.innerHTML = `$${20*value}`
 
